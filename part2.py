781ec829aafe120e4d9a30983341ca91d975ed43
zahimizrahi <zahimizrahi1@gmail.com>
2020-08-01 19:00:29 +0300

    added part2

--- part2.py
+++ part2.py
@@ -1,11 +1,7 @@
-import os
-
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Dense, Dropout, Flatten, Embedding, multiply, Concatenate, LeakyReLU, BatchNormalization
-from tensorflow.keras.optimizers import Adam, SGD
-from sklearn import metrics
 
 
 class Part2Generator():
